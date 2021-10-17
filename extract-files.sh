LineageOS/android_device_samsung_universal8890-common

universal8890: Switch to patchelf from tools-lineage
Change-Id: If1a7328804fb9bc39ccec70690f1090f2d0509b3

extract-files.sh
@@ -74,14 +74,14 @@ sed -i "s|system/etc|vendor/etc|g" $BLOB_ROOT/vendor/lib64/libsec-ril-dsds.so
 sed -i "s|system/etc|vendor/etc|g" $BLOB_ROOT/vendor/lib64/libsec-ril.so
 
 # Replace protobuf with vndk29 compat libs for specified libs
-patchelf --replace-needed libprotobuf-cpp-full.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib/libwvhidl.so
-patchelf --replace-needed libprotobuf-cpp-full.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib/mediadrm/libwvdrmengine.so
+"${PATCHELF}" --replace-needed libprotobuf-cpp-full.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib/libwvhidl.so
+"${PATCHELF}" --replace-needed libprotobuf-cpp-full.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib/mediadrm/libwvdrmengine.so
 
 # Remove wpa_supplicant service from wifi.rc
 sed -i "41,51d" $BLOB_ROOT/vendor/etc/init/wifi_sec.rc
 
 # Replace libvndsecril-client with libsecril-client
-patchelf --replace-needed libvndsecril-client.so libsecril-client.so $BLOB_ROOT/vendor/lib/libwrappergps.so
-patchelf --replace-needed libvndsecril-client.so libsecril-client.so $BLOB_ROOT/vendor/lib64/libwrappergps.so
+"${PATCHELF}" --replace-needed libvndsecril-client.so libsecril-client.so $BLOB_ROOT/vendor/lib/libwrappergps.so
+"${PATCHELF}" --replace-needed libvndsecril-client.so libsecril-client.so $BLOB_ROOT/vendor/lib64/libwrappergps.so
 
 "$MY_DIR"/setup-makefiles.sh

ExyDEP/device_samsung_universal8890-common

extract-files.sh
@@ -70,14 +70,14 @@ sed -i "s|system/etc|vendor/etc|g" $BLOB_ROOT/vendor/lib/libfloatingfeature.so
 sed -i "s|system/etc|vendor/etc|g" $BLOB_ROOT/vendor/lib64/libfloatingfeature.so
 
 # Replace protobuf with vndk29 compat libs for specified libs
-patchelf --replace-needed libprotobuf-cpp-lite.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib/libwvhidl.so
-patchelf --replace-needed libprotobuf-cpp-lite.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib/mediadrm/libwvdrmengine.so
+"${PATCHELF}" --replace-needed libprotobuf-cpp-lite.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib/libwvhidl.so
+"${PATCHELF}" --replace-needed libprotobuf-cpp-lite.so libprotobuf-cpp-lite-v29.so $BLOB_ROOT/vendor/lib/mediadrm/libwvdrmengine.so
 
 # Remove wpa_supplicant service from wifi.rc
 sed -i "41,51d" $BLOB_ROOT/vendor/etc/init/wifi_sec.rc
 
 # Replace libvndsecril-client with libsecril-client
-patchelf --replace-needed libvndsecril-client.so libsecril-client.so $BLOB_ROOT/vendor/lib/libwrappergps.so
-patchelf --replace-needed libvndsecril-client.so libsecril-client.so $BLOB_ROOT/vendor/lib64/libwrappergps.so
+"${PATCHELF}" --replace-needed libvndsecril-client.so libsecril-client.so $BLOB_ROOT/vendor/lib/libwrappergps.so
+"${PATCHELF}" --replace-needed libvndsecril-client.so libsecril-client.so $BLOB_ROOT/vendor/lib64/libwrappergps.so
 
 "$MY_DIR"/setup-makefiles.sh