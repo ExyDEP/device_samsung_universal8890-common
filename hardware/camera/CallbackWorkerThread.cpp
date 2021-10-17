LineageOS/android_device_samsung_universal8890-common

universal8890: camera: resolve all Clang compilation warnings
Change-Id: I37fd31b141f36589e0dd1034a279746e8c0f6436

hardware/camera/CallbackWorkerThread.cpp
@@ -126,7 +126,7 @@ void CallbackWorkerThread::ClearCallbacks() {
     /* Lock the mutex and clear the message queue */
     std::unique_lock<std::mutex> lk(m_mutex);
 
-    ALOGV("%s: Clearing %i messages", __FUNCTION__, m_queue.size());
+    ALOGV("%s: Clearing %lu messages", __FUNCTION__, m_queue.size());
 
     /* Whilst the queue is not empty */
     while (!m_queue.empty()) {

ExyDEP/device_samsung_universal8890-common

hardware/camera/CallbackWorkerThread.cpp
@@ -126,7 +126,7 @@ void CallbackWorkerThread::ClearCallbacks() {
     /* Lock the mutex and clear the message queue */
     std::unique_lock<std::mutex> lk(m_mutex);
 
-    ALOGV("%s: Clearing %i messages", __FUNCTION__, m_queue.size());
+    ALOGV("%s: Clearing %zu messages", __FUNCTION__, m_queue.size());
 
     /* Whilst the queue is not empty */
     while (!m_queue.empty()) {