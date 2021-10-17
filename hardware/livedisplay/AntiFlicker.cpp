LineageOS/android_device_samsung_universal8890-common

universal8890: livedisplay: Add anti flicker support
Change-Id: Ieb91a3efe0fc124e120f95f5ec197aa3c5cecb14

hardware/livedisplay/AntiFlicker.cpp
@@ -1,0 +1,62 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/file.h>
+#include <android-base/strings.h>
+
+#include <fstream>
+
+#include "AntiFlickerExynos.h"
+
+using android::base::ReadFileToString;
+using android::base::Trim;
+using android::base::WriteStringToFile;
+
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace V2_0 {
+namespace universal8890 {
+
+static constexpr const char* kDcDimmingStatusPath = "/sys/class/lcd/panel/smart_on";
+
+// Methods from ::vendor::lineage::livedisplay::V2_0::IAntiFlicker follow.
+bool AntiFlickerExynos::isSupported() {
+    std::fstream file(kDcDimmingStatusPath, file.in | file.out);
+    return file.good();
+}
+
+// Methods from ::vendor::lineage::livedisplay::V2_0::IAntiFlicker follow.
+Return<bool> AntiFlickerExynos::isEnabled() {
+    std::string tmp;
+    int32_t contents = 0;
+
+    if (ReadFileToString(kDcDimmingStatusPath, &tmp)) {
+        contents = std::stoi(Trim(tmp));
+    }
+
+    return contents > 0;
+}
+
+Return<bool> AntiFlickerExynos::setEnabled(bool enabled) {
+    return WriteStringToFile(enabled ? "1" : "0", kDcDimmingStatusPath, true);
+}
+
+}  // namespace universal8890
+}  // namespace V2_0
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor

hardware/livedisplay/AntiFlicker.h
@@ -1,0 +1,55 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VENDOR_LINEAGE_LIVEDISPLAY_V2_0_ANTIFLICKER_H
+#define VENDOR_LINEAGE_LIVEDISPLAY_V2_0_ANTIFLICKER_H
+
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+#include <vendor/lineage/livedisplay/2.0/IAntiFlicker.h>
+
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace V2_0 {
+namespace universal8890 {
+
+using ::android::hardware::hidl_array;
+using ::android::hardware::hidl_memory;
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::sp;
+
+class AntiFlickerExynos : public IAntiFlicker {
+  public:
+    bool isSupported();
+
+    // Methods from ::vendor::lineage::livedisplay::V2_0::IAntiFlicker follow.
+    Return<bool> isEnabled() override;
+    Return<bool> setEnabled(bool enabled) override;
+
+    // Methods from ::android::hidl::base::V1_0::IBase follow.
+};
+
+}  // namespace universal8890
+}  // namespace V2_0
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
+
+#endif  // VENDOR_LINEAGE_LIVEDISPLAY_V2_0_ANTIFLICKEREXYNOS_H

hardware/livedisplay/service.cpp
@@ -21,6 +21,7 @@
 #include <hidl/HidlTransportSupport.h>
 
 #include "AdaptiveBacklight.h"
+#include "AntiFlicker.h"
 #include "DisplayColorCalibration.h"
 #include "DisplayModes.h"
 #include "ReadingEnhancement.h"
@@ -33,6 +34,7 @@ using android::status_t;
 using android::OK;
 
 using vendor::lineage::livedisplay::V2_0::universal8890::AdaptiveBacklight;
+using vendor::lineage::livedisplay::V2_0::samsung::AntiFlicker;
 using vendor::lineage::livedisplay::V2_0::universal8890::DisplayColorCalibration;
 using vendor::lineage::livedisplay::V2_0::universal8890::DisplayModes;
 using vendor::lineage::livedisplay::V2_0::universal8890::ReadingEnhancement;
@@ -40,6 +42,7 @@ using vendor::lineage::livedisplay::V2_0::universal8890::SunlightEnhancement;
 
 int main() {
     sp<AdaptiveBacklight> adaptiveBacklight;
+    sp<AntiFlicker> antiFlicker;
     sp<DisplayColorCalibration> displayColorCalibration;
     sp<DisplayModes> displayModes;
     sp<ReadingEnhancement> readingEnhancement;
@@ -52,6 +55,13 @@ int main() {
     if (adaptiveBacklight == nullptr) {
         LOG(ERROR)
             << "Can not create an instance of LiveDisplay HAL AdaptiveBacklight Iface, exiting.";
+        goto shutdown;
+    }
+
+    antiFlicker = new AntiFlickerExynos();
+    if (antiFlicker == nullptr) {
+        LOG(ERROR)
+            << "Can not create an instance of LiveDisplay HAL AntiFlicker Iface, exiting.";
         goto shutdown;
     }
 
@@ -89,6 +99,16 @@ int main() {
         if (status != OK) {
             LOG(ERROR) << "Could not register service for LiveDisplay HAL AdaptiveBacklight Iface ("
                        << status << ")";
+            goto shutdown;
+        }
+    }
+
+    if (antiFlicker->isSupported()) {
+        status = antiFlicker->registerAsService();
+        if (status != OK) {
+            LOG(ERROR)
+                << "Could not register service for LiveDisplay HAL AntiFlicker Iface ("
+                << status << ")";
             goto shutdown;
         }
     }

ExyDEP/device_samsung_universal8890-common

hardware/livedisplay/AntiFlicker.cpp
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <android-base/file.h>
+#include <android-base/strings.h>
+
+#include <fstream>
+
+#include "AntiFlicker.h"
+
+using android::base::ReadFileToString;
+using android::base::Trim;
+using android::base::WriteStringToFile;
+
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace V2_0 {
+namespace implementation {
+
+static constexpr const char* kDcDimmingStatusPath = "/sys/class/lcd/panel/smart_on";
+
+// Methods from ::vendor::lineage::livedisplay::V2_0::IAntiFlicker follow.
+Return<bool> AntiFlicker::isEnabled() {
+    std::string tmp;
+    int32_t contents = 0;
+
+    if (ReadFileToString(kDcDimmingStatusPath, &tmp)) {
+        contents = std::stoi(Trim(tmp));
+    }
+
+    return contents > 0;
+}
+
+Return<bool> AntiFlicker::setEnabled(bool enabled) {
+    return WriteStringToFile(enabled ? "1" : "0", kDcDimmingStatusPath, true);
+}
+
+}  // namespace implementation
+}  // namespace V2_0
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor

hardware/livedisplay/AntiFlicker.h
@@ -1,0 +1,54 @@
+/*
+ * Copyright (C) 2019 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef VENDOR_LINEAGE_LIVEDISPLAY_V2_0_ANTIFLICKER_H
+#define VENDOR_LINEAGE_LIVEDISPLAY_V2_0_ANTIFLICKER_H
+
+#include <hidl/MQDescriptor.h>
+#include <hidl/Status.h>
+#include <vendor/lineage/livedisplay/2.0/IAntiFlicker.h>
+
+namespace vendor {
+namespace lineage {
+namespace livedisplay {
+namespace V2_0 {
+namespace implementation {
+
+using ::android::hardware::hidl_array;
+using ::android::hardware::hidl_memory;
+using ::android::hardware::hidl_string;
+using ::android::hardware::hidl_vec;
+using ::android::hardware::Return;
+using ::android::hardware::Void;
+using ::android::sp;
+
+class AntiFlicker : public IAntiFlicker {
+  public:
+
+    // Methods from ::vendor::lineage::livedisplay::V2_0::IAntiFlicker follow.
+    Return<bool> isEnabled() override;
+    Return<bool> setEnabled(bool enabled) override;
+
+    // Methods from ::android::hidl::base::V1_0::IBase follow.
+};
+
+}  // namespace implementation
+}  // namespace V2_0
+}  // namespace livedisplay
+}  // namespace lineage
+}  // namespace vendor
+
+#endif  // VENDOR_LINEAGE_LIVEDISPLAY_V2_0_ANTIFLICKEREXYNOS_H

hardware/livedisplay/service.cpp
@@ -32,6 +32,8 @@ using android::sp;
 using android::status_t;
 using android::OK;
 
+using vendor::lineage::livedisplay::V2_0::IAntiFlicker;
+using vendor::lineage::livedisplay::V2_0::implementation::AntiFlicker;
 using vendor::lineage::livedisplay::V2_0::IDisplayColorCalibration;
 using vendor::lineage::livedisplay::V2_0::implementation::DisplayColorCalibration;
 using vendor::lineage::livedisplay::V2_0::IDisplayModes;
@@ -42,6 +44,7 @@ using vendor::lineage::livedisplay::V2_0::ISunlightEnhancement;
 using vendor::lineage::livedisplay::V2_0::implementation::SunlightEnhancement;
 
 int main() {
+    sp<IAntiFlicker> antiFlicker;
     sp<IDisplayColorCalibration> displayColorCalibration;
     sp<IDisplayModes> displayModes;
     sp<IReadingEnhancement> readingEnhancement;
@@ -49,6 +52,13 @@ int main() {
     status_t status;
 
     LOG(INFO) << "LiveDisplay HAL service is starting.";
+
+    antiFlicker = new AntiFlicker();
+    if (antiFlicker == nullptr) {
+        LOG(ERROR)
+            << "Can not create an instance of LiveDisplay HAL AntiFlicker Iface, exiting.";
+        goto shutdown;
+    }
 
     displayColorCalibration = new DisplayColorCalibration();
     if (displayColorCalibration == nullptr) {
@@ -79,6 +89,14 @@ int main() {
     }
 
     configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    status = antiFlicker->registerAsService();
+    if (status != OK) {
+        LOG(ERROR)
+            << "Could not register service for LiveDisplay HAL AntiFlicker Iface ("
+            << status << ")";
+        goto shutdown;
+    }
 
     status = displayColorCalibration->registerAsService();
     if (status != OK) {