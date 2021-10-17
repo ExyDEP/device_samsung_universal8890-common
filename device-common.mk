fcuzzocrea/android_device_samsung_universal8890-common

universal8890: Exclude overlay/hardware/samsung/AdvancedDisplay from RRO

device-common.mk
@@ -23,7 +23,8 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     device/samsung/universal8890-common/overlay/lineage-sdk \
     device/samsung/universal8890-common/overlay/packages/apps/Snap \
     device/samsung/universal8890-common/overlay/frameworks/base/packages/SystemUI \
-    device/samsung/universal8890-common/overlay/packages/apps/Bluetooth
+    device/samsung/universal8890-common/overlay/packages/apps/Bluetooth \
+    device/samsung/universal8890-common/overlay/hardware/samsung/AdvancedDisplay
 
 # Boot animation
 TARGET_BOOTANIMATION_PRELOAD := true

ExyDEP/device_samsung_universal8890-common

Change-Id: Id612d051188859586c0ca15c692ef7b9ffcb1003

device-common.mk
@@ -21,7 +21,8 @@ DEVICE_PACKAGE_OVERLAYS += $(COMMON_PATH)/overlay
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     device/samsung/universal8890-common/overlay/lineage-sdk \
-    device/samsung/universal8890-common/overlay/packages/apps/Snap
+    device/samsung/universal8890-common/overlay/packages/apps/Snap \
+    device/samsung/universal8890-common/overlay/hardware/samsung/AdvancedDisplay
 
 # Boot animation
 TARGET_BOOTANIMATION_PRELOAD := true